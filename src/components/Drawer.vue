94c1a0bdab1ba8ca2a6f1824c52549ea7bf79b28
Jrb Youssef <jrb.youssef@gmail.com>
2021-05-20 15:19:49 +0100

    Install vuetify

---
+++ src/components/Drawer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    app
+  >
+    <!--  -->
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Drawer',
+  data: () => ({ drawer: null }),
+}
+</script>
